d2560d4436ef661eef6fec3ef7a5f1f3e1cf73a5
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-06-08 09:56:31 +0000

    synchronous and asynchronous

---
+++ 01_Basics/06_today.js
@@ -0,0 +1,10 @@
+// console.log("frist");
+// setTimeout(()=>{
+//     console.log("Sceond");
+// },3000)
+// console.log("3rd")
+//-----------------------------------------------------
+function orderPizza() {
+    setTimeout(function () { console.log("order paced"); }, 2000);
+}
+orderPizza();
---
+++ 01_Basics/06_today.ts
@@ -0,0 +1,16 @@
+// console.log("frist");
+
+
+// setTimeout(()=>{
+//     console.log("Sceond");
+// },3000)
+
+// console.log("3rd")
+
+//----------------------------------------------time limit set in function-----------------------------------------------------
+
+function orderPizza (){
+    setTimeout(()=>{console.log("order paced") },2000)
+}
+
+orderPizza()
