ef31c213c937341e5ac1d2c0b9142aa24593f8f2
ibrarhassan00 <167034360+ibrarhassan00@users.noreply.github.com>
2024-06-22 22:28:36 +0000

    update

--- 01_Basics/06_today.js
+++ 01_Basics/06_today.js
@@ -4,13 +4,29 @@
 // },3000)
 // console.log("3rd")
 //----------------------------------------------time limit set in function-----------------------------------------------------
-function orderPizza(prepareFizza) {
-    setTimeout(function () {
-        console.log("order paced");
-        prepareFizza();
-    }, 2000);
-}
-function prepareFizza() {
-    setTimeout(function () { console.log("prepared pizza"); }, 2000);
-}
-orderPizza(prepareFizza);
+// function orderPizza (prepareFizza){
+//     setTimeout(()=>{console.log("order paced");
+//         prepareFizza()
+//      },2000)
+// }
+"newone";
+// function prepareFizza (){
+//     setTimeout(()=>{console.log("prepared pizza")},2000)
+// }
+var House = /** @class */ (function () {
+    function House(name, ads) {
+        this.numberOfDoor = 5;
+        this.onwerName = name;
+        this.address = ads;
+    }
+    House.prototype.bell = function () {
+        console.log(this.address, "Ding Dong");
+    };
+    return House;
+}());
+var h1 = new House("ibrar", "G1234");
+h1.bell();
+var h2 = new House("noor", " f2334");
+h2.bell();
+var h3 = new House("naz", "G345");
+h3.bell();
--- 01_Basics/06_today.ts
+++ 01_Basics/06_today.ts
@@ -9,15 +9,38 @@
 
 //----------------------------------------------time limit set in function-----------------------------------------------------
 
-function orderPizza (prepareFizza){
-    setTimeout(()=>{console.log("order paced");
-        prepareFizza()
-     },2000)
-}
-
-function prepareFizza (){
-    setTimeout(()=>{console.log("prepared pizza")},2000)
+// function orderPizza (prepareFizza){
+//     setTimeout(()=>{console.log("order paced");
+//         prepareFizza()
+//      },2000)
+// }
+"newone";
+// function prepareFizza (){
+//     setTimeout(()=>{console.log("prepared pizza")},2000)
     
-}
+// }
 
-orderPizza(prepareFizza)
+
+class House {
+    onwerName : String;
+   readonly address : string ;
+    numberOfDoor: number = 5;
+    constructor(name : string , ads : string ){this.onwerName = name ; 
+        this.address=ads;
+    }
+
+         bell() {
+            console.log(this.address , "Ding Dong");
+        }
+
+}
+let h1 = new House ("ibrar","G1234");
+h1. bell()
+
+let h2 = new House ("noor" , " f2334");
+h2.bell()
+
+let h3 = new House ("naz" , "G345");
+h3.bell()
+
+
